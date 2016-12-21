735c15ea3073e4a39c37fbb74469a077f3f0e7cf
Vincent Fontaine <ftechhelp@gmail.com>
2016-12-20 23:59:34 -0500

    Added InputManager Class to get mouse click and keyboard key events

---
+++ SSA/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using Gma.System.MouseKeyHook;
+using System.Windows.Forms;
+
+namespace SSA
+{
+    class InputManager
+    {
+
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public InputManager()
+        {
+            m_GlobalHook = null;
+        }
+
+        public void Subscribe()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //DO SOMETHING
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            //DO SOMETHING
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommeneded to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+    }
+}
