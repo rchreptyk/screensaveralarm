88c7cc474c5bdcb67a67e1e2c0dfb178ad493869
Russell Chreptyk <russell@chreptyk.com>
2016-12-22 11:12:04 -0800

    Added full screen support

---
+++ SSA/ImageViewer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSA
+{
+    public partial class ImageViewer : Form
+    {
+        /// <summary>
+        /// Creates a window that displays an image full screen
+        /// </summary>
+        /// <param name="image">The image to show</param>
+        public ImageViewer(Image image)
+        {
+            InitializeComponent();
+            BackgroundImage = image;
+        }
+    }
+}
