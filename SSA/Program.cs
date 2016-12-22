5785a5bcca9f8bf483352ecd0523710591f88d5b
Russell Chreptyk <russell@chreptyk.com>
2016-12-22 10:53:18 -0800

    Removed testing code

--- SSA/Program.cs
+++ SSA/Program.cs
@@ -12,15 +12,7 @@ namespace SSA
     {
         static void Main(string[] args)
         {
-            //TESTING PURPOSES//
-            InputManager im = new InputManager();
-            im.Subscribe();
-            ScreenManager sm = new ScreenManager();
-            Form form = new Form();
-            form.BackgroundImage = sm.getScreenshot();
-            form.Size = new Size(500, 500);
-            form.ShowDialog();
-
+            
         }
     }
 }
