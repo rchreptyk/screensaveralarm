b92f96fa51a9c0e8e818c11f90268ed01747b609
Vincent Fontaine <ftechhelp@gmail.com>
2016-12-27 15:57:45 -0500

    Fixed Scaling Issue

---
+++ SSA/ScreenShot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SSA
+{
+    class ScreenShot
+    {
+
+        public ScreenShot()
+        {
+        }
+
+        public Bitmap CaptureMainScreen()
+        {
+            int screenWidth = (int)(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size.Width);
+            int screenHeight = (int)(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size.Height);
+            Bitmap bmp_with_actual_resolution = new Bitmap(screenWidth, screenHeight);
+            Graphics g = Graphics.FromImage(bmp_with_actual_resolution);
+            g.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+            return bmp_with_actual_resolution;
+        }
+
+    }
+}
